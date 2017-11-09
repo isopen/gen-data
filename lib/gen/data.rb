4ae3f49586aceb36b66c7d8095a2e5cc6ebf0045
Maxim Tyulenev <mr_makss@mail.ru>
2017-11-09 23:50:29 +0700

    ref

--- lib/gen/data.rb
+++ lib/gen/data.rb
@@ -2,6 +2,34 @@ require "gen/data/version"
 
 module Gen
   module Data
-    # Your code goes here...
+    def Data.request_data(r)
+      c = []
+      len = r.length;
+      case len
+      when 1
+        c << r[0]
+      when 2
+        c << r[0]
+        c << r[1]
+        c << r[0].merge(r[1])
+      when (3..len)
+        c << r[0]
+        c << r[1]
+        c << r[2]
+        c << r[0].merge(r[1])
+        c << r[0].merge(r[2])
+        c << r[1].merge(r[2])
+        c << r[0].merge(r[1]).merge(r[2])
+        t = r[3, len]
+        t.length.times { |j|
+          c.length.times { |i|
+            c << c[i].dup.merge(t[j])
+          }
+          c << t[j]
+        }
+        c << {}
+      end  
+      return c;
+    end
   end
 end
